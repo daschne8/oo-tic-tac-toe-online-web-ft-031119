adbd5201fa61e5974cf602f12a9d6526c9f6e0a1
Daniel Schneider <daschne8@gmail.com>
2019-08-08 16:08:18 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,5 +1,16 @@
 class TicTacToe
 
+  WIN_COMBINATIONS = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [6,4,2],
+    [0,4,8]
+  ]
+
   def initialize
     @board = 9.times.collect{|n| ""}
   end
